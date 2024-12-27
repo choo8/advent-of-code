76cd51cda888d06e31208d5ffe214579f6303dab
choo8 <choo8@illinois.edu>
2024-12-27 03:50:19 +0000

    Completed AOC 2024 Day 1 with Python

---
+++ 2024/python/day1/day1.py
@@ -0,0 +1,37 @@
+from collections import Counter
+
+
+def parse(filename: str):
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    l1, l2 = map(list, zip(*[l.split() for l in lines]))
+    return l1, l2
+
+
+def part1():
+    for filename in ["example.txt", "input.txt"]:
+        l1, l2 = parse(filename)
+
+        l1.sort()
+        l2.sort()
+
+        total_dist = sum(map(lambda x: abs(int(x[0]) - int(x[1])), zip(l1, l2)))
+
+        print(f"[Part 1] Total distance for {filename}: {total_dist}")
+
+
+def part2():
+    for filename in ["example.txt", "input.txt"]:
+        l1, l2 = parse(filename)
+
+        c2 = Counter(l2)
+        
+        sim_score = sum(map(lambda x: int(x) * c2[x], l1))
+
+        print(f"[Part 2] Similarity score for {filename}: {sim_score}")
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
